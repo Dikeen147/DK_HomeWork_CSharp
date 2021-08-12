f62f6dd83646991767567a9063936315fe217bcd
Konstantin <ConstAnto147@yandex.ru>
2021-08-12 11:08:33 +0300

    W6_T1 updated delegate on two double parameter

---
+++ CSharpBasics/Webinar_6/W6_T1_DelegateFunc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace W6_T1_DelegateFunc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DelegateFunc.StartDemo();
+        }
+    }
+}
