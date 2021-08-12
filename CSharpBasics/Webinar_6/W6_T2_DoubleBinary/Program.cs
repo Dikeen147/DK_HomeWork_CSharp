e3ada84fb09da95b808c02527bf9ac49de3db417
Konstantin <ConstAnto147@yandex.ru>
2021-08-12 12:14:03 +0300

    W6_T2 updated DoubleBinary class

---
+++ CSharpBasics/Webinar_6/W6_T2_DoubleBinary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace W6_T2_DoubleBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoubleBinary.StartDemo("data.bin");
+        }
+    }
+}
