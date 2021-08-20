ba1533749b858123d6c9baaf34ca41a73ff897d0
Konstantin <ConstAnto147@yandex.ru>
2021-08-20 15:06:41 +0300

    W8_T1 Show DateTime all properties

---
+++ CSharpBasics/Webinar_8/W8_T1_DateTimeReflection/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace W8_T1_DateTimeReflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var item in new DateTime().GetType().GetProperties())
+                Console.WriteLine($"{item,30} | {item.Name}");
+        }
+    }
+}
