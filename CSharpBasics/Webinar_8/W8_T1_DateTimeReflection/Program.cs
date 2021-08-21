2bc4f3a50ff75e6b0e72730a055802c4a7fd325d
Konstantin <ConstAnto147@yandex.ru>
2021-08-21 14:11:20 +0300

    W8_T1 Added task description

--- CSharpBasics/Webinar_8/W8_T1_DateTimeReflection/Program.cs
+++ CSharpBasics/Webinar_8/W8_T1_DateTimeReflection/Program.cs
@@ -2,6 +2,12 @@
 
 namespace W8_T1_DateTimeReflection
 {
+    /*
+     Работа Долгова Константина
+
+    Вебинар 8. Задача 1.
+    С помощью рефлексии выведите все свойства структуры DateTime
+     */
     class Program
     {
         static void Main(string[] args)
